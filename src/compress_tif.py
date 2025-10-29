f8aec2e2c99171519565736579569019fbbd55fd
Alán F. Muñoz <amunozgo@broadinstitute.org>
2025-10-29 12:27:47 -0400

    feat: use zarr2 format when using numcodecs

--- src/compress_tif.py
+++ src/compress_tif.py
@@ -1,16 +1,19 @@
 import lzma
 from itertools import groupby
 from pathlib import Path
+from shutil import rmtree
+from time.time import perf_counter
 
-import numcodecs
 import numpy
 import zarr
 from imagecodecs.numcodecs import Brotli, Jpegxl
-from numcodecs import LZMA, Blosc
 from PIL import Image
+from zarr.codecs import BloscCodec
 
 input_dir = Path("/home/amunoz/projects/JUMP_core/src/images/raw")
 output_dir = Path("./images")
+
+overwrite = True
 
 output_dir.mkdir(parents=True, exist_ok=True)
 
@@ -24,16 +27,19 @@ filters = [
 
 # compression_algs = {"jpegxl": Jpegxl}
 compressing_algs = {
+    # "lz4": {"clevel": 9},
     "lz4hc": {"clevel": 9},
     "zstd": {"clevel": 9},
+    "zlib": {"clevel": 9},
 }
 compressors_blosc = {
-    k: Blosc(cname=k, shuffle=-1, **v) for k, v in compressing_algs.items()
+    k: BloscCodec(cname=k, shuffle="bitshuffle", **v)
+    for k, v in compressing_algs.items()
 }
 
 compressors = {
     "brotli": Brotli(level=11),
-    "jpegxl": Jpegxl,
+    "jpegxl": Jpegxl(),
     **compressors_blosc,
 }
 # imagecodecs_compresso  # rs = [
@@ -56,34 +62,51 @@ groups = {
     for k, g in groupby(sorted(input_dir.glob("*.tif"), key=key_fn), key=key_fn)
 }
 # %%
+
+compression_time = {}
+decompression_time = {}
 for name, compressor in compressors.items():
-    numcodecs.register_codec(compressor)
+    # numcodecs.register_codec(compressor)
     store_name = Path(output_dir) / f"{name}.zarr"
-    if store_name.exists():
-        print(f"Skipping {name}")
-        continue
+    if store_name.exists():  # To overwrite
+        if overwrite:
+            rmtree(store_name)
+        else:
+            print(f"Skipping {name}")
+            continue
+
+    t_start = perf_counter()
     store = zarr.storage.LocalStore(store_name)
-    root = zarr.create_group(store=store)
+    zarr_format = 3
+    if not isinstance(compressor, zarr.codecs.blosc.BloscCodec):
+        zarr_format = 2
+    root = zarr.create_group(store=store, zarr_format=zarr_format)
     for key, items in groups.items():
         site_name = "__".join(key)
         nchannels = len(items)
         example_arr = numpy.array(Image.open(items[0]))
         shape = example_arr.shape
         dtype = example_arr.dtype
-        root.create_array(
+
+        # The API for codecs changed with Zarr 3
+        # https://github.com/cgohlke/imagecodecs/issues/123
+        arr = zarr.create_array(
+            store=store,
             name=site_name,
             shape=(nchannels, *shape),
             chunks=(nchannels, *shape),
             dtype=dtype,
+            compressors=compressor,
+            zarr_format=zarr_format,
         )
-
+        tmp_arr = numpy.zeros((nchannels, *shape))
         for i, img_path in enumerate(items):
-            img = numpy.array(Image.open(img_path))
-            root[site_name][i] = img
+            tmp_arr[i] = numpy.array(Image.open(img_path))
 
-            # if site_name not in root:
-            #     root.create_group(site_name)
-        # arr = zarr.array(img, chunks=img.shape)
-        # root[site_name][i] = img
+        arr[:] = tmp_arr
 
-# from itertools import groupby
+    compression_time[name] = perf_counter() - t_start
+
+    # TODO add decompression test
+
+    # TODO Add size
